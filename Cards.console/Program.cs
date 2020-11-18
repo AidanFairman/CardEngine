bb9ee98aaf679be0a02118f1ebf5606d13c2d7c5
AidanFairman <aidantfairman@gmail.com>
2020-11-18 01:54:54 +0000

    working on setting up the project

---
+++ Cards.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cards.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
