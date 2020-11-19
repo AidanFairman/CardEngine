49bea28fbb224ab229bac7d1d6c68eaf6bded147
AidanFairman <aidantfairman@gmail.com>
2020-11-19 00:28:40 +0000

    dod some work, changing to local VSCode

---
+++ Cards/Card/CardBase.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace Cards.Card{
+    public class CardBase{
+        Dictionary<string, Delegate> actions = new Dictionary<string, Delegate>();
+        
+    }
+}
